0b35b4be0f62b219328b34b0a9367a762dc9de56
Suraj Budha Thoki <surajbudha@yahoo.co.in>
2025-11-20 15:10:20 +0100

    fixed code using pyllint

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
 #!/usr/bin/env python
-import gnupg
-from getpass import getpass
-
-pass_phrase = getpass("Enter Passphrase: ")
-
-gpg = gnupg.GPG()
+"""Module providing a function for gpg key creation"""
+from getpass import getpass #  to securely get passphrase input
+import gnupg # to interact with GPG
 
 
-# generate key
-input_data = gpg.gen_key_input(
-    key_type='RSA',
-    key_length=2048,
-    name_email='surajbudha@yahoo.co.in',
-    passphrase=pass_phrase,
-    name_real='Suraj Budha Thoki',
-    expire_date='5y',
-    key_usage='sign,encrypt,auth',
-    #algorithm='rsa4096'
-)
-key = gpg.gen_key(input_data)
-print(key)
 
-# create ascii-readable versions of pub / private keys
-ascii_armored_public_keys = gpg.export_keys(key.fingerprint)
-ascii_armored_private_keys = gpg.export_keys(
-    keyids=key.fingerprint,
-    secret=True,
-    passphrase=pass_phrase,
-)
 
-# export
-with open('mykeyfile.asc', 'w') as f:
-    f.write(ascii_armored_public_keys)
-    f.write(ascii_armored_private_keys)
+
+def main(): # main function
+    """Main function to create gpg key pair and export to ascii-armored file"""
+    gpg = gnupg.GPG()
+    pass_phrase = getpass("Enter Passphrase: ")
+
+    # generate key
+    input_data = gpg.gen_key_input(
+        key_type='RSA',
+        key_length=2048,
+        name_email='surajbudha@yahoo.co.in',
+        passphrase=pass_phrase,
+        name_real='Suraj Budha Thoki',
+        expire_date='5y',
+        key_usage='sign,encrypt,auth',
+        #algorithm='rsa4096'
+    )
+    key = gpg.gen_key(input_data)
+    print(key)
+
+    # create ascii-readable versions of pub / private keys
+    ascii_armored_public_keys = gpg.export_keys(key.fingerprint)
+    ascii_armored_private_keys = gpg.export_keys(
+        keyids=key.fingerprint,
+        secret=True,
+        passphrase=pass_phrase,
+    )
+
+    # export
+    with open('mykeyfile.asc', 'w', encoding="utf-8") as f:
+        f.write(ascii_armored_public_keys)
+        f.write(ascii_armored_private_keys)
+
+if __name__ == '__main__':
+    main()
